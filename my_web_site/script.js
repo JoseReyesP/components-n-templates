a342ab7eecfb04e2e801b05a46f8d1303ad30c43
JoseReyesP <relujo@hotmail.com>
2024-11-21 19:51:39 -0500

    menu bottons style ready

--- my_web_site/script.js
+++ my_web_site/script.js
@@ -5,14 +5,16 @@ menuButtons.forEach((menuButton) => {
     if (menuButton.classList.contains("unactive")) {
       menuButton.classList.remove("unactive");
       menuButton.classList.add("active");
-      console.log;
+      const newIcon = menuButton.querySelector("img").id;
+      menuButton.querySelector("img").src = `icons/${newIcon}_dark.svg`;
     }
     const toDeactivate = menuButtons.filter((el) => el.id !== menuButton.id);
-    console.log(toDeactivate);
     toDeactivate.forEach((el) => {
       if (el.classList.contains("active")) {
         el.classList.remove("active");
         el.classList.add("unactive");
+        const newIcon = el.querySelector("img").id;
+        el.querySelector("img").src = `icons/${newIcon}_light.svg`;
       }
     });
   });
